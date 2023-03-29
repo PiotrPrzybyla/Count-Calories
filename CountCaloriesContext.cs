3bad88a4c34474c02384fc2b5cf30322c49bfe86
Radosz <radosz.werner@gmail.com>
2023-03-29 19:05:00 +0200

    adding example of meal.

--- CountCaloriesContext.cs
+++ CountCaloriesContext.cs
@@ -6,11 +6,11 @@ namespace Count_Calories
 {
     public class CountCaloriesContext : DbContext
     {
-        // Your context has been configured to use a 'CountCaloriesContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'Count_Calories.CountCaloriesContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'CountCaloriesContext' 
+        // Your context has been configured to use a 'CountCaloriesContext' connection string from your application's
+        // configuration file (App.config or Web.config). By default, this connection string targets the
+        // 'Count_Calories.CountCaloriesContext' database on your LocalDb instance.
+        //
+        // If you wish to target a different database and/or database provider, modify the 'CountCaloriesContext'
         // connection string in the application configuration file.
         public CountCaloriesContext()
             : base("name=CountCaloriesContext")
@@ -25,7 +25,7 @@ namespace Count_Calories
 
         public DbSet<Product> Products { get; set; }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // Add a DbSet for each entity type that you want to include in your model. For more information
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
         // public virtual DbSet<MyEntity> MyEntities { get; set; }
@@ -35,16 +35,15 @@ namespace Count_Calories
     {
         protected override void Seed(CountCaloriesContext context)
         {
-            var Meals = new List<Meal>
+            var meals = new List<Meal>
                 {
                     new Meal()
                     {
                         Id=1001,
                         Name="Carbonaura",
-
                     }
                 };
-            var UserMeals = new List<UserMeal>
+            var userMeals = new List<UserMeal>
             {
                  new UserMeal()
                  {
@@ -54,23 +53,37 @@ namespace Count_Calories
                      MealType = UserMeal.MealTypeENUM.Lunch,
                  }
             };
-            var Products = new List<Product>
+            var products = new List<Product>
             {
                 new Product()
                 {
-
-
+                    Name="Parmegiano Regiano",
+                    Id=5001,
+                    Calories=900,
+                    Carbs=5,
+                    Fat = 600,
+                    Protein=1,
                 }
-
-
+            };
+            var ingredients = new List<Ingredient>
+            {
+                new Ingredient()
+                {
+                    Id=4001,
+                    MealId = 1001,
+                    ProductId = 5001,
+                    IngredientWeight = 30,
+                }
             };
 
-            UserMeals.ForEach(s => context.UserMeals.Add(s));
+            userMeals.ForEach(s => context.UserMeals.Add(s));
+
+            meals.ForEach(s => context.Meals.Add(s));
+
+            products.ForEach(s => context.Products.Add(s));
+
+            ingredients.ForEach(s => context.Ingredients.Add(s));
             context.SaveChanges();
         }
-
-
-
-
     }
 }
--- Product.cs
+++ Product.cs
@@ -4,15 +4,13 @@ namespace Count_Calories
 {
     public class Product
     {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-
         public int Calories { get; set; }
         public int Carbs { get; set; }
         public int Fat { get; set; }
-        public int Protein { get; set; }
+        public int Id { get; set; }
 
         public virtual ICollection<Ingredient> Ingredients { get; set; }
+        public string Name { get; set; }
+        public int Protein { get; set; }
     }
 }
